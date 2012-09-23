3617faf2c0935b20604a2c05069be5979580197a
Andrew Kubera <andrewkubera@gmail.com>
2012-09-23 15:42:03 -0400

    HtmlElement now stores classes in a list. Splits names into .classes now

--- htmlelement.php
+++ htmlelement.php
@@ -13,60 +13,117 @@ class HtmlElement {
 
     public $type;
     var $children = [];
-    var $attributes = [];
     var $parent = null;
     var $text = '';
+    static $default_tag_name = "div";
+
+    protected $attributes = [];
 
     public function __construct($type, $attributes = [], $text = "", $selfclose = false) {
         if (type === "") {
             throw new Exception("Type must not be empty string");
         }
+        // Removes an id from the tag name (i.e. "span#my_id")
+        $id = $this->extractID($type);
 
-        // Break type string around the '#'
-        $a = explode("#", $type);
-
-        // If we lead with #idname - default type to div
-        if ($a[0] === "" && $a[1]) {
-            $a[0] = "div";
-        }
-
-        $b = explode('.',$a[0]);
+        // Removes all classes and stores into a list 
+        $classes = $this->extractClasses($type);
         
-        $type = $b[0];
-        $b = \array_slice($b, 1);
+        
+        if ($id !== null) {
+            $this->setAttribute('id', $id);
+        }
 
-        // a[1] now has everything after '#' char 
-        if ($a[1]) {
-            $idlist = explode('.', $a[1]);
-            // the first element is the id
-            $idlist[0] && $attributes['id'] = $idlist[0];
-            $b = array_merge($b, array_slice($idlist, 1));
+        if ($classes !== []) {
+            $this->AddClasses($classes);
         }
-        $groups = $b;
-        if ($groups !== []) {
-            // initilize to blank string if not already set
-            !$attributes['class'] && $attributes['class'] = "";
-            $attributes['class'] .= implode(" ", $groups);
-        }
+
         $this->type = $type;
         $this->selfclosing = $selfclose;
-        if (is_array($attributes)) {
-            foreach ($attributes as $key => $value) {
-                $this->setAttribute($key, $value);
-            }
-        }
+        $this->setAttribute($attributes);
         $this->addText($text);
     }
 
+    private function extractID(&$str) {
+        $id = NULL;
+
+        // Break type string around the '#'
+        $a = explode("#", $str);
+
+        // If we lead with #idname use default type (auto div)
+        if ($a[0] === "" && $a[1]) {
+            $a[0] = self::$default_tag_name;
+        }
+
+        // a[1] now has everything after '#' char 
+        if ($a[1]):
+            // $break is now index of delimeter
+            $break = strpos($a[1], '.');
+
+            //  no delimeter found, copy everything to id
+            if ($break === false) {
+                $id = $a[1];
+                $str = $a[0];
+            } else {
+                $id = substr($a[1], 0, $break);
+                $str = $a[0] . substr($a[1], $break);
+            }
+        endif;
+        return $id;
+    }
+
+    private function extractClasses(&$str) {
+        $class_list = explode('.', $str);
+        $str = array_shift($class_list);
+        return $class_list;
+    }
+
+    public function AddClass($class_name) {
+        if (!$this->attributes['class']) {
+            $this->attributes['class'] = [];
+        }
+        $this->attributes['class'][] = $class_name;
+    }
+
+    public function AddClasses($classes) {
+        if (!$this->attributes['class']) {
+            $this->attributes['class'] = [];
+        }
+       $this->attributes['class'] += $classes;
+    }
+    
+    /**
+     * 
+     * @param String $class_name
+     */
+    public function RemoveClass($class_name) {
+        $this->attributes['class'] = array_diff($this->attributes['class'], [$class_name]);
+    }
+
+    /**
+     * 
+     * @param Array $classes
+     */
+    public function RemoveClasses($classes) {
+        $this->attributes['class'] = array_diff($this->attributes['class'], $classes);
+    }
+
     public function addAttribute($attribute, $value = '') {
+        if ($attribute === 'class') {
+            $this->AddClass($value);
+        }
         $this->setAttribute($attribute, $value);
         return $this;
     }
 
     public function setAttribute($attribute, $value = '') {
         if (is_array($attribute)) {
-            $this->attributes = array_merge($this->attributes, $attribute);
-        } else {
+            foreach ($attribute as $key => $value) {
+                $this->setAttribute($key, $value);
+            }
+        } else if ($attribute == 'class') {
+            $this->AddClass($value);
+        }else {
             $this->attributes[$attribute] = $value;
         }
     }
@@ -103,12 +160,22 @@ class HtmlElement {
     }
 
     public function __toString() {
-        if ($this->type == '') {
+        if ($this->type === '') {
             return $this->text;
         }
         $result = "<{$this->type}";
+        
+        // compress the array of classes to a string
+        if (isset($this->attributes['class'])) {
+            $tmp_classlist = $this->attributes['class'];
+            $this->attributes['class'] = implode(' ', $tmp_classlist);
+        }
         foreach ($this->attributes as $key => $value) {
             $result .= " {$key}='{$value}'";
+        }
+        // return classes to the stored array
+        if (isset($this->attributes['class'])) {
+            $this->attributes['class'] = $tmp_classlist;
         }
 
         if ($this->selfclosing) {
