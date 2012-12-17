8cfe31bc80a8cd5d3871b573ff31bb15783ed624
Andrew Kubera <andrewkubera@gmail.com>
2012-12-17 00:27:29 -0500

    Added some plisp files for eventual inclusion.

---
+++ plisp/list.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace plisp;
+
+class plisp_list {
+    
+}
---
+++ plisp/plisp.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace plisp;
+
+global $plisp_registry;
+$plisp_registry = [];
+
+require_once 'list.php';
+require_once 'token.php';
+
+class PLISP {
+    
+    public function __construct() {
+    }
+    
+    /**
+     * 
+     * @param string $str
+     * @return array 
+     */
+    static public function tokenize($str) {
+        $res = [];
+        
+        $str;
+        return $res;
+    }
+}
