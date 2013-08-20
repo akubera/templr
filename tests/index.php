31df928b7e555a56971e716729914c158996953d
Andrew Kubera <andrewkubera@gmail.com>
2013-08-20 16:32:55 -0400

    Added an empty page for running/rendering tests

---
+++ tests/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<?php
+/*
+ * tests/index.php
+ *
+ */
+
+?>
+<html>
+<head>
+  <title>Templr Tests</title>
+  <meta charset="utf-8">
+</head>
+<body>
+   <h2>Templr Tests</h2>
+</body>
+</html>
+
